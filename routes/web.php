9313204aca433aa7711dcf759a841aeb35c28def
Osman Altunbağ <osmanaltunbag@gmail.com>
2026-03-02 13:03:12 +0300

    Redirect root to login/dashboard

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,10 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    if (auth()->check()) {
+        return redirect()->route('dashboard');
+    }
+    return redirect()->route('login');
 });
 
 // Google OAuth routes
