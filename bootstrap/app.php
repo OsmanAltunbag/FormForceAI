9d4ac6fd73bbd447994787547a9f702ac20fd50e
Osman Altunbağ <osmanaltunbag@gmail.com>
2026-03-02 17:09:25 +0300

    Update bootstap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Render gibi platformlarda HTTPS trafiğini doğru algılamak için
+        $middleware->trustProxies(at: '*'); 
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
